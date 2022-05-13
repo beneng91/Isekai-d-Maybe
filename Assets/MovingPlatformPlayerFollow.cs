d70e1cf584311679e9b4c62c8a06932f361a708e
beneng91 <103171571+beneng91@users.noreply.github.com>
2022-05-13 15:56:22 +1000

    Moving Platform

---
+++ Assets/MovingPlatformPlayerFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformPlayerFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("I hit something...");
+        if(collision.gameObject.tag == "PlayerBody" || collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player has hit me!");
+            collision.gameObject.transform.parent = gameObject.transform;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "PlayerBody" || collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player got off me!");
+            collision.gameObject.transform.parent = null;
+        }
+    }
+
+}
